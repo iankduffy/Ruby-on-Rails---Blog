b9748aa46065a7518acd74191f6990490d949c84
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-06-08 19:39:12 +0100

    index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
+
   def homepage
+    @articles = Article.all.limit(3)
   end
 
   def about
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#homepage'
-  get 'about', to: 'pages#about'
   resources :articles
+  get 'about', to: 'pages#about'
+  root 'pages#homepage'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
