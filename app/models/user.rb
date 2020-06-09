6c6103c7795ee78303b8fa0c2120f57b4bc9f894
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-06-09 14:44:46 +0100

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presense: true, 
+                    uniquness: { case_sensitive: false }, 
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX}
+  validates :username, presense: true, 
+                      uniquness: { case_sensitive: false }, 
+                      length: { minimum: 3, maximum: 25 }
+                      
 end
