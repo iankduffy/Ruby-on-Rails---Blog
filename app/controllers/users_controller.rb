88ed2b5ada2199a38094a5f354a51f71fd54003e
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-06-09 17:35:33 +0100

    add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # def create
-  #   @article = Article.new(article_params)
-  #   @article.user = User.first
-  #   if @article.save
-  #     flash[:notice] = "Article was created successful"
-  #     redirect_to @article
-  #   else 
-  #     render 'new'
-  #   end 
-  # end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User was created successful"
+      redirect_to articles_path
+    else 
+      render 'new'
+    end 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
