6bc7b5c68a88b09b30633f63e1e378e5ea7f7c96
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-06-09 16:48:27 +0100

    added password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save = { self.email = email.downcase }
+  before_save { self.email = email.downcase }
   has_many :articles
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }, 
                       length: { minimum: 3, maximum: 25 }
-                      
+  has_secure_password
 end
