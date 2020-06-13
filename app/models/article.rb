c7501da1fb90295161b94a7f9f35ee1328b2d6c9
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-06-13 01:02:15 +0100

    added categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :articles_categories
+  has_many :categories, through: :articles_categories
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates_uniqueness_of :name
+  has_many :articles_categories
+  has_many :articles, through: :articles_categories
 end
