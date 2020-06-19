3adbff9db0d0aad643ebe1ed60104a7f7f18ad31
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-06-19 16:08:12 +0100

    url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'https://rails-image-blog.s3-eu-west-1.amazonaws.com/'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
